220d1b7893ef99ddd4301e14bf8916b015c05db0
TakutoNamba <namba@w0w.co.jp>
2023-02-13 18:21:22 +0900

    render texture に stencil texture をコピー

--- AR_depthImage_0127/Assets/Scripts/PostEffect.cs
+++ AR_depthImage_0127/Assets/Scripts/PostEffect.cs
@@ -17,12 +17,54 @@ public class PostEffect : MonoBehaviour
     void checkStencilTex()
     {
         Texture2D stencilTex = occlusionManager.humanStencilTexture;
-        Color[] pixels = stencilTex.GetPixels();
+
+        // Create a temporary RenderTexture of the same size as the texture
+        RenderTexture tmp = RenderTexture.GetTemporary(
+                            stencilTex.width,
+                            stencilTex.height,
+                            0,
+                            RenderTextureFormat.Default,
+                            RenderTextureReadWrite.Linear);
+
+
+        // Blit the pixels on texture to the RenderTexture
+        Graphics.Blit(stencilTex, tmp);
+
+
+        // Backup the currently set RenderTexture
+        RenderTexture previous = RenderTexture.active;
+
+
+        // Set the current RenderTexture to the temporary one we created
+        RenderTexture.active = tmp;
+
+
+        // Create a new readable Texture2D to copy the pixels to it
+        Texture2D myTexture2D = new Texture2D(stencilTex.width, stencilTex.height);
+
+
+        // Copy the pixels from the RenderTexture to the new Texture
+        myTexture2D.ReadPixels(new Rect(0, 0, tmp.width, tmp.height), 0, 0);
+        myTexture2D.Apply();
+
+
+        // Reset the active RenderTexture
+        RenderTexture.active = previous;
+
+
+        // Release the temporary RenderTexture
+        RenderTexture.ReleaseTemporary(tmp);
+
+
+        // "myTexture2D" now has the same pixels from "texture"
+
+
+        Color[] pixels = myTexture2D.GetPixels();
         Color[] change_pixels = new Color[pixels.Length];
         for (int i = 0; i < pixels.Length; i++)
         {
             Color pixel = pixels[i];
-            if(pixel.r != 0 && pixel.g != 0 && pixel.b != 0)
+            if (pixel.r != 0 || pixel.g != 0 || pixel.b != 0)
             {
                 isThereHuman = true;
                 break;
